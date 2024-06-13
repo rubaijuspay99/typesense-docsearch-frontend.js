8f497414601ab21a35e495d411c14db09320f244
Rubai <rubai.mandal@juspay.in>
2024-06-13 17:00:12 +0530

    Remove refresh

--- packages/docsearch-react/src/Hit.tsx
+++ packages/docsearch-react/src/Hit.tsx
@@ -7,24 +7,6 @@ interface HitProps {
   children: React.ReactNode;
 }
 
-// export function Hit({ hit, children }: HitProps) {
-//   const handleLinkClick = () => {
-//     window.location.reload();
-//   };
-//   return <a href={hit.url} onClick={handleLinkClick}>{children}</a>;
-// }
-
-
 export function Hit({ hit, children }: HitProps) {
-  const handleLinkClick = () => {
-    // Redirect to the URL specified in the hit object
-    window.location.href = hit.url;
-  };
-
-  return <div className="replace-a-tag" onClick={handleLinkClick}>{children}</div>;
+  return <a href={hit.url}>{children}</a>;
 }
-
-
-// export function Hit({ hit, children }: HitProps) {
-//   return <a href={hit.url}>{children}</a>;
-// }
